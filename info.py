fbcd78cb3ea56730c2eafdaea5393e55a5c7ec62
maran117 <153261389+maran117@users.noreply.github.com>
2024-11-27 18:24:05 +0500

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '') # request channel id ( make 
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hi:mWirsMP0ELYvNBn6@cluster0.1ybqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mangoDB")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
 

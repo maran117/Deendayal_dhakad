9e2fc9ad4564ced3d0f5faba88ebbf3bf7404f90
maran11 <55264858+maran11@users.noreply.github.com>
2024-11-27 16:27:52 +0500

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://chemical-hippopotamus-tamilflixmv1-08a95be6.koyeb.app/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
